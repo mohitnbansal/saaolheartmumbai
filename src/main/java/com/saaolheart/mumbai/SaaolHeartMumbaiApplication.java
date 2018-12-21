07caf05a9899860285d18db0e754229785aa20b0
Mohit Bansal <mohitnbansal@gmail.com>
2018-12-21 07:27:09 +0530

    Inital Commit

---
+++ src/main/java/com/saaolheart/mumbai/SaaolHeartMumbaiApplication.java
@@ -0,0 +1,14 @@
+package com.saaolheart.mumbai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaaolHeartMumbaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaaolHeartMumbaiApplication.class, args);
+	}
+
+}
+
